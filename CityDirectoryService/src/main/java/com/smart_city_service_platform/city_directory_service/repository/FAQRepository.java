77aa3571b10e07a6717ec5fab08d3f93046c7b8e
Justyna Puz <puzjustyna@gmail.com>
2025-03-21 11:24:00 +0100

    add FAQ JPA repositoy

---
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/repository/FAQRepository.java
@@ -0,0 +1,15 @@
+package com.smart_city_service_platform.city_directory_service.repository;
+
+
+import com.smart_city_service_platform.city_directory_service.model.FAQ;
+import com.smart_city_service_platform.city_directory_service.model.FAQCategory;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FAQRepository extends JpaRepository<FAQ, Long> {
+  List<FAQ> findByCategory(FAQCategory category);
+
+}
