f7aeade830429a7a7301d0c0375172ab3fef8f8f
Justyna Puz <puzjustyna@gmail.com>
2025-03-21 11:59:14 +0100

    add main app file

---
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/CityDirectoryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.smart_city_service_platform.city_directory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+		"com.smart_city_service_platform.city_directory_service.service",
+		"com.smart_city_service_platform.city_directory_service.controller"
+})
+public class CityDirectoryServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CityDirectoryServiceApplication.class, args);
+	}
+}
