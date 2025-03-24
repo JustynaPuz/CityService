2b433bdcf47e8e8477af6e8ba8e4a7d4780c558a
Justyna Puz <puzjustyna@gmail.com>
2025-03-24 13:10:25 +0100

    add model files for common

--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/Address.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/Address.java
@@ -1,22 +1,27 @@
-//package com.smart_city_service_platform.city_directory_service.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.Getter;
-//
-//@Entity
-////@Table(name = "address")
-//@Getter
-//class Address {
-//
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long id;
-//  private String city;
-//  private String country;
-//  private String street;
-//  private String houseNumber;
-//}
+package com.smart_city_service_platform.city_directory_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String city;
+  private String country;
+  private String street;
+  private String houseNumber;
+}
--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/WorkingHours.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/WorkingHours.java
@@ -1,22 +1,27 @@
-//package com.smart_city_service_platform.city_directory_service.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import java.sql.Time;
-//import java.time.LocalTime;
-//import lombok.Getter;
-//
-//@Entity
-////@Table(name = "working_hours")
-//@Getter
-//class WorkingHours {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private LocalTime from;
-//    private LocalTime to;
-//
-//}
+package com.smart_city_service_platform.city_directory_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Time;
+import java.time.LocalTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "working_hours")
+@Getter
+@Setter
+@NoArgsConstructor
+public class WorkingHours {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalTime fromTime;
+    private LocalTime toTime;
+
+}
