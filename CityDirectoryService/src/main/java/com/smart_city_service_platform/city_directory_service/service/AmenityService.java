6bcea93ec102ec8727f5096c6e51a7ff97201677
Justyna Puz <puzjustyna@gmail.com>
2025-03-24 13:47:48 +0100

    add amenity service

--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/service/AmenityService.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/service/AmenityService.java
@@ -1,34 +1,52 @@
-//package com.smart_city_service_platform.city_directory_service.service;
-//
-//import com.smart_city_service_platform.city_directory_service.model.Amenity;
-//import com.smart_city_service_platform.city_directory_service.repository.AmenityRepository;
-//import java.util.List;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-////@RequiredArgsConstructor
-//public class AmenityService {
-//
-//  private final AmenityRepository repository;
-//
-//  public AmenityService(AmenityRepository repository) {
-//    this.repository = repository;
-//  }
-//
-//  public Amenity save(Amenity amenity) {
-//    return repository.save(amenity);
-//  }
-//
-//  public List<Amenity> findAll() {
-//    return repository.findAll();
-//  }
-//
-//  public List<Amenity> findByCategory(String category) {
-//    return repository.findByCategory(category);
-//  }
-//
-//  public List<Amenity> findByLocation(String location) {
-//    return repository.findByLocation(location);
-//  }
-//}
+package com.smart_city_service_platform.city_directory_service.service;
+
+import com.smart_city_service_platform.city_directory_service.model.Amenity;
+import com.smart_city_service_platform.city_directory_service.model.AmenityCategory;
+import com.smart_city_service_platform.city_directory_service.repository.AmenityRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AmenityService {
+
+  private final AmenityRepository repository;
+
+  public AmenityService(AmenityRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Amenity> getAllAmenities() {
+    return repository.findAll();
+  }
+
+  public Amenity getAmenityById(Long id) {
+    return repository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Amenity not found with id: " + id));
+  }
+
+  public List<Amenity> getAmenitiesByCategory(AmenityCategory category) {
+    return repository.findByCategory(category);
+  }
+
+  public List<Amenity> getAmenitiesByLocation(Long locationId) {
+    return repository.findByLocation_Id(locationId);
+  }
+
+  public Amenity createAmenity(Amenity amenity) {
+    return repository.save(amenity);
+  }
+
+  public Amenity updateAmenity(Long id, Amenity updatedAmenity) {
+    Amenity existingAmenity = getAmenityById(id);
+    existingAmenity.setName(updatedAmenity.getName());
+    existingAmenity.setCategory(updatedAmenity.getCategory());
+    existingAmenity.setLocation(updatedAmenity.getLocation());
+    existingAmenity.setDetails(updatedAmenity.getDetails());
+    return repository.save(existingAmenity);
+  }
+
+  public void deleteAmenity(Long id) {
+    repository.deleteById(id);
+  }
+}
