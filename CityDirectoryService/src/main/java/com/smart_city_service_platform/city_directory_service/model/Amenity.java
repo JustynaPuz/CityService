c3bfb5078d3d74882ee471c68ccf404d07db5b03
jpuz <jpuz@griddynamics.com>
2025-03-24 13:41:45 +0100

    change amenity model

--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/Amenity.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/Amenity.java
@@ -1,29 +1,42 @@
-//package com.smart_city_service_platform.city_directory_service.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Entity
-//@Table(name = "amenity")
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Amenity extends Facility {
-//
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long id;
-//  private AmenityCategory category;
-//
-//  public Amenity(AmenityCategory category) {
-//    this.category = category;
-//  }
-//
-//}
+package com.smart_city_service_platform.city_directory_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "amenity")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Amenity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @Enumerated(EnumType.STRING)
+  private AmenityCategory category;
+
+  @ManyToOne
+  @JoinColumn(name = "location_id")
+  private Location location;
+
+  @OneToOne
+  @JoinColumn(name = "details_id")
+  private FacilityDetails details;
+
+
+}
--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/AmenityCategory.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/AmenityCategory.java
@@ -1,8 +1,8 @@
-//package com.smart_city_service_platform.city_directory_service.model;
-//
-//public enum AmenityCategory {
-//  PARK,
-//  LIBRARY,
-//  HOSPITAL
-//
-//}
+package com.smart_city_service_platform.city_directory_service.model;
+
+public enum AmenityCategory {
+  PARK,
+  LIBRARY,
+  HOSPITAL
+
+}
