7dc0513175fc5fd3595f4bd7fb11d0212ce7ecb8
jpuz <jpuz@griddynamics.com>
2025-03-24 13:10:25 +0100

    add model files for common

--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/ContactDetails.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/ContactDetails.java
@@ -1,21 +1,25 @@
-//package com.smart_city_service_platform.city_directory_service.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.Getter;
-//
-//@Entity
-////@Table(name = "contact_details")
-//class ContactDetails {
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long id;
-//  @Getter
-//  private String phoneNumber;
-//  @Getter
-//  private String email;
-//
-//}
+package com.smart_city_service_platform.city_directory_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "contact_details")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ContactDetails {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String phoneNumber;
+  private String email;
+
+}
--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/FacilityDetails.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/model/FacilityDetails.java
@@ -1,24 +1,32 @@
-//package com.smart_city_service_platform.city_directory_service.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.OneToOne;
-//import jakarta.persistence.Table;
-//import lombok.Getter;
-//
-//@Entity
-////@Table(name = "facility_details")
-//class FacilityDetails {
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long id;
-//  @Getter
-//  @OneToOne
-//  private WorkingHours workingHours;
-//  @Getter
-//  @OneToOne
-//  private Address address;
-//
-//}
+package com.smart_city_service_platform.city_directory_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "facility_details")
+@Getter
+@Setter
+@NoArgsConstructor
+public class FacilityDetails {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne
+  @JoinColumn(name = "working_hours_id")
+  private WorkingHours workingHours;
+
+  @OneToOne
+  @JoinColumn(name = "address_id")
+  private Address address;
+
+}
