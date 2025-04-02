e992a5211de0a1ba38803577ec8fe6384ee92ac4
Justyna Puz <puzjustyna@gmail.com>
2025-04-02 11:27:49 +0200

    clean code changes

--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/mapper/FaqMapper.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/mapper/FaqMapper.java
@@ -1,14 +1,17 @@
 package com.smart_city_service_platform.city_directory_service.mapper;
 
-import com.smart_city_service_platform.city_directory_service.DTO.FaqDTO;
+import com.smart_city_service_platform.city_directory_service.DTO.FAQResponse;
+import com.smart_city_service_platform.city_directory_service.DTO.FaqRequestDTO;
 import com.smart_city_service_platform.city_directory_service.model.FAQ;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface FaqMapper {
 
-  FAQ toEntity(FaqDTO dto);
+  FAQ toEntity(FaqRequestDTO dto);
 
-  FaqDTO toDTO(FAQ entity);
+  FaqRequestDTO toDTO(FAQ entity);
+
+  FAQResponse toResponse(FAQ entity);
 
 }
