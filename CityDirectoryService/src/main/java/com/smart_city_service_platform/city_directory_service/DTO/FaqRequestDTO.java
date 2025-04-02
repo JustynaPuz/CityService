bde2a6a4975b1867c68c62d49a645a355ebb1a93
jpuz <jpuz@griddynamics.com>
2025-04-02 11:27:03 +0200

    rename file

--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/DTO/FaqRequestDTO.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/DTO/FaqRequestDTO.java
@@ -2,15 +2,13 @@ package com.smart_city_service_platform.city_directory_service.DTO;
 
 import com.smart_city_service_platform.city_directory_service.model.FAQCategory;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class FaqDTO {
+@Data
+public class FaqRequestDTO {
   private String question;
   private String answer;
   private FAQCategory category;
