ec81aafcda21d311585f6f760c32561fd8903434
jpuz <jpuz@griddynamics.com>
2025-04-02 13:01:30 +0200

    fix pacakge scan

--- CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/CityDirectoryServiceApplication.java
+++ CityDirectoryService/src/main/java/com/smart_city_service_platform/city_directory_service/CityDirectoryServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-		"com.smart_city_service_platform.city_directory_service"
+		"com.smart_city_service_platform.city_directory_service.service",
+		"com.smart_city_service_platform.city_directory_service.controller",
+		"com.smart_city_service_platform.city_directory_service.mapper",
+		"com.smart_city_service_platform.city_directory_service.DTO"
 })
 public class CityDirectoryServiceApplication {
 	public static void main(String[] args) {
