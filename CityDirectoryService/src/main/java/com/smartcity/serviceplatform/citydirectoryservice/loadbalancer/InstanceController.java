41cb93a09dcc916609e00c07b84cb1c4e5d8f07b
Justyna Puz <puzjustyna@gmail.com>
2025-04-18 13:01:29 +0200

    change dependency

--- CityDirectoryService/src/main/java/com/smartcity/serviceplatform/citydirectoryservice/loadbalancer/InstanceController.java
+++ CityDirectoryService/src/main/java/com/smartcity/serviceplatform/citydirectoryservice/loadbalancer/InstanceController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InstanceController {
 
-  private final String instanceId = UUID.randomUUID().toString();
-
   @GetMapping("/instance-id")
   public String getInstanceId() {
     return "I'm running on: " + System.getenv("INSTANCE_NAME");
