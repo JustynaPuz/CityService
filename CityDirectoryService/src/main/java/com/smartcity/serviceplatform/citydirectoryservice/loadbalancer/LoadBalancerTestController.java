014c0ac5987d66e3f23bf5589ca52717046ab4d2
jpuz <jpuz@griddynamics.com>
2025-04-24 12:41:53 +0200

    add logstash

--- CityDirectoryService/src/main/java/com/smartcity/serviceplatform/citydirectoryservice/loadbalancer/LoadBalancerTestController.java
+++ CityDirectoryService/src/main/java/com/smartcity/serviceplatform/citydirectoryservice/loadbalancer/LoadBalancerTestController.java
@@ -1,5 +1,7 @@
 package com.smartcity.serviceplatform.citydirectoryservice.loadbalancer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +10,8 @@ import org.springframework.web.client.RestTemplate;
 public class LoadBalancerTestController {
 
   private final RestTemplate restTemplate;
+  private static final Logger log = LoggerFactory.getLogger(LoadBalancerTestController.class);
+
 
   public LoadBalancerTestController(RestTemplate restTemplate) {
     this.restTemplate = restTemplate;
@@ -15,7 +19,12 @@ public class LoadBalancerTestController {
 
   @GetMapping("/test-loadbalancer")
   public String test() {
+    log.info("Calling CITY-DIRECTORY-SERVICE at /instance-id");
+
     String response = restTemplate.getForObject("http://CITY-DIRECTORY-SERVICE/instance-id", String.class);
+
+    log.info("Received response from instance: {}", response);
+
     return "Response from: " + response;
   }
 }
